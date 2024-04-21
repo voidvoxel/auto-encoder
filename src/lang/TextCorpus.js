975537937f3be7f347426c1126497b32a3c103e7
Ashlynn "Void Voxel" Juniper <160202125+voidvoxel@users.noreply.github.com>
2024-04-20 22:08:28 -0400

    Fix bugs

--- src/lang/TextCorpus.js
+++ src/lang/TextCorpus.js
@@ -1,8 +1,26 @@
-const getLongestWord = require("./getLongestWord");
-const getWordList = require("./getWordList");
+const {
+    getLongestWord,
+    getWordList,
+    isValidSourceText
+ } = require("./functions");
 
 
-class TextCorpus extends Array {
+function invalidArgumentError (
+    parameterName,
+    parameterType,
+    value
+) {
+    throw new TypeError(
+        "Invalid argument [ "
+            + `${parameterName}: ${typeof parameterType} `
+            + `= ${value} (${typeof value}) `
+            + " ]"
+
+    );
+}
+
+
+class TextCorpus {
     /**
      * Create a `Corpus` from a `CorpusLike` object.
      * @param {import("./TextCorpusLike")} corpusLike
@@ -10,15 +28,43 @@ class TextCorpus extends Array {
      * The new `Corpus`.
      */
     static from (corpusLike) {
-        if (typeof corpusLike === 'string') {
+        if (TextCorpus._isTextCorpus(corpusLike)) {
+            return corpusLike;
+        } else if (isValidSourceText(corpusLike)) {
             return new TextCorpus(corpusLike);
-        } else if (typeof corpusLike === 'object') {
-            if (corpusLike instanceof TextCorpus) {
-                return corpusLike;
-            } else if (corpusLike instanceof Array) {
-                return new TextCorpus(corpusLike);
-            }
+        } else {
+            throw invalidArgumentError(
+                'corpusLike',
+                'TextCorpusLike',
+                corpusLike
+            );
         }
+    }
+
+
+    /**
+     * Return `true` if `corpusLike` is a `Corpus`.
+     * Otherwise, returns `false`.
+     * @param {import("./TextCorpusLike")} corpusLike
+     * The object in question.
+     * @returns {boolean}
+     * Whether or not `corpusLike` is a `Corpus`.
+     * @private
+     */
+    static _isTextCorpus (corpusLike) {
+        if (typeof corpusLike !== 'object') {
+            return false;
+        }
+
+        if (corpusLike === null) {
+            return false;
+        }
+
+        if (corpusLike instanceof TextCorpus) {
+            return true;
+        }
+
+        return false;
     }
 
 
@@ -28,22 +74,30 @@ class TextCorpus extends Array {
      * One or more bodies of text to create a `Corpus` from.
      */
     constructor (text) {
-        // Get the list of text entries.
-        let entries = text;
-
-        if (typeof entries === 'string') {
-            entries = [ entries ];
+        if (TextCorpus._isTextCorpus(text)) {
+            text = structuredClone(
+                text.flat(Number.POSITIVE_INFINITY)
+            );
         }
 
-        // Initialize the `Array` aspect of the `Corpus`.
-        super(entries);
+        if (!isValidSourceText(text)) {
+            throw invalidArgumentError(
+                'text',
+                'TextCorpusLike',
+                text
+            );
+        }
 
-        // Populate the caches.
-        this.longestWord();
-        this.uniqueWords();
+        this._entries = structuredClone(
+            text.flat(Number.POSITIVE_INFINITY)
+        );
 
-        // Freeze the object once the caches are populated.
-        Object.freeze(this);
+        this._initialize(text);
+    }
+
+
+    entries () {
+        return this._entries;
     }
 
 
@@ -53,8 +107,7 @@ class TextCorpus extends Array {
      * The longest word found within the `Corpus`.
      */
     longestWord () {
-        return this._longestWord
-            ??= getLongestWord(this._words);
+        return this._longestWord ??= getLongestWord(this._uniqueWords);
     }
 
 
@@ -64,12 +117,33 @@ class TextCorpus extends Array {
      * A list of all unique words found within the `Corpus`.
      */
     uniqueWords () {
-        this._uniqueWords
-            ??= getWordList(this._entries);
-
-        const uniqueWords = structuredClone(this._uniqueWords);
+        const uniqueWords = this._uniqueWords ??= getWordList(this);
 
         return uniqueWords;
+    }
+
+
+    toJSON () {
+        return Object.values(this);
+    }
+
+
+    _initialize (entries) {
+        if (typeof entries === 'string') {
+            entries = [ entries ];
+        }
+
+        // Initialize the `Array` aspect of the `Corpus`.
+        for (let entry of entries) {
+            this._entries.push(entry);
+        }
+
+        // Populate the caches.
+        this.uniqueWords();
+        this.longestWord();
+
+        // Freeze the object once the caches are populated.
+        Object.freeze(this);
     }
 }
 
